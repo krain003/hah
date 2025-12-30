74720f95df113554f447e41c176ffe7749dfa731
Krain <krain@example.com>
2025-12-30 23:41:25 +0300

    FORCE RE-ADD ALL PROJECT FILES

---
+++ handlers/__init__.py
@@ -0,0 +1,14 @@
+# handlers/__init__.py
+from .start import router as start_router
+from .wallet import router as wallet_router
+from .send import router as send_router
+from .receive import router as receive_router
+from .swap import router as swap_router
+from .p2p import router as p2p_router
+from .history import router as history_router
+from .settings import router as settings_router
+
+__all__ = [
+    "start_router", "wallet_router", "send_router", "receive_router",
+    "swap_router", "p2p_router", "history_router", "settings_router"
+]
